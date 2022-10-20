ce63f7e1884693fcc56f8768b73984ca442305ac
jamienichols <jamie337nichols@gmail.com>
2022-10-20 05:33:06 -0400

    update

--- src/pages/Main/components/maincontent/MainContent.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-export default function MainContent() {
-  return (
-    <div className="w-full h-full flex flex-col justify-center items-center gap-4">
-      <h1 className="text-4xl">Welcome to the main page!</h1>
-      <p className="text-2xl">This is the main page of the app.</p>
-    </div>
-  )
-}
