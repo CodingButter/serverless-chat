de278afe4fdf83b5eee90b573cdd78381ff6737b
jamienichols <jamie337nichols@gmail.com>
2022-10-25 04:43:24 -0400

    Added Images

--- src/assets/server_templates/default_template.ts
+++
@@ -1,8 +0,0 @@
-import NewServerIcon from '../icons/new_server_default.png'
-import defaultChannels from './channel_templates/default_channels'
-
-export default {
-  label: 'Create my own',
-  icon: NewServerIcon,
-  channels: [...defaultChannels]
-}
