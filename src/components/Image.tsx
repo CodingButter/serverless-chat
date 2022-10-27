b749b8d551eba5f2d1b6451f4c7458f041e17bf6
jamienichols <jamie337nichols@gmail.com>
2022-10-27 17:14:04 -0400

    testing large images

--- src/components/Image.tsx
+++ src/components/Image.tsx
@@ -1,3 +1,4 @@
+import classNames from 'classnames'
 import React, { Suspense, useRef } from 'react'
 
 function Spinner() {
@@ -8,21 +9,22 @@ interface ImageProps extends React.ComponentPropsWithRef<'img'> {
   src: string
   fallback?: React.ReactNode | null
   alt: string
+  className?: string
 }
 
-export default function Image({ src, fallback, alt, ...rest }: ImageProps) {
+export default function Image({ src, fallback, className, alt, ...rest }: ImageProps) {
   const [loaded, setLoaded] = React.useState(false)
   const loadingComponent =
     fallback ||
     ((
-      <div className="w-full h-full flex justify-center items-center">
+      <div className="w-full h-full flex justify-center items-center z-10 relative">
         <Spinner />
       </div>
     ) as React.ReactNode)
   return (
     <>
       {!loaded && loadingComponent}
-      <img onLoad={() => setLoaded(true)} src={src} alt={alt} {...rest} />
+      <img onLoad={() => setLoaded(true)} src={src} alt={alt} {...rest} className={`${!loaded && 'hidden'}`} />
     </>
   )
 }
